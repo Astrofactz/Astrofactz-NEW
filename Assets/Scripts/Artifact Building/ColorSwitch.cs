c1c47f7561e6f4cba90c6a80b5a6d35c3fd4906b
snakeweber <jake.weber72@gmail.com>
2022-04-17 15:53:16 -0500

    Fragment Highlighting

--- Assets/Scripts/Artifact Building/ColorSwitch.cs
+++ Assets/Scripts/Artifact Building/ColorSwitch.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class ColorSwitch : MonoBehaviour
 {
-    private Material original;
-    public Material selected;
+    //private Material original;
+    //public Material selected;
+
+    public GameObject highlight;
+    public GameObject outline;
 
     private void Start()
     {
-        original = GetComponent<Renderer>().material;
+        //original = GetComponent<Renderer>().material;
+        highlight.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,12 +31,28 @@ public class ColorSwitch : MonoBehaviour
 
     private void OnMouseDrag()
     {
-        GetComponent<Renderer>().material = selected;
+        //GetComponent<Renderer>().material = selected;
+        highlight.SetActive(true);
+        outline.SetActive(false);
+    }
+
+    private void OnMouseOver()
+    {
+        highlight.SetActive(true);
+        outline.SetActive(false);
+    }
+
+    private void OnMouseExit()
+    {
+        highlight.SetActive(false);
+        outline.SetActive(true);
     }
 
     private void OnMouseUp()
     {
-        GetComponent<Renderer>().material = original;
+        //GetComponent<Renderer>().material = original;
+        highlight.SetActive(false);
+        outline.SetActive(true);
     }
 
 }
