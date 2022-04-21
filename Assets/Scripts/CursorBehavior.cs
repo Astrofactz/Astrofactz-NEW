f5622ab1f8d0a8fb5c07c9984f5f573b1ac0a461
anthonypollos <apollos@mail.bradley.edu>
2022-04-21 11:46:49 -0700

    Cursor fixes + added click cursor to menus

--- Assets/Scripts/CursorBehavior.cs
+++ Assets/Scripts/CursorBehavior.cs
@@ -7,6 +7,8 @@
                     mouse is clicked.
 *******************************************************************************/
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class CursorBehavior : MonoBehaviour
 {
@@ -21,6 +23,11 @@ public class CursorBehavior : MonoBehaviour
     public Texture2D inGameCursorPressed;
 
     /// <summary>
+    /// The cursor used when holding down left click in the menus
+    /// </summary>
+    public Texture2D inGameCursorTap;
+
+    /// <summary>
     /// Forces cursor to render as the selected sprite
     /// </summary>
     public CursorMode cursorMode = CursorMode.Auto;
@@ -30,24 +37,32 @@ public class CursorBehavior : MonoBehaviour
     /// </summary>
     public Vector2 hotspot = Vector2.zero;
 
+    public bool isMainMenuScenes;
+
     /// <summary>
     /// Called at start; sets mouse cursor sprite
     /// </summary>
-    private void Start()
+    public void Start()
     {
         Cursor.SetCursor(inGameCursor, hotspot, cursorMode);
+
+
     }
 
     /// <summary>
     /// Called every frame; detects mouse button inputs
     /// </summary>
-    private void Update()
+    public void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.GetMouseButtonDown(0) && isMainMenuScenes == false)
             Cursor.SetCursor(inGameCursorPressed, hotspot, cursorMode);
 
+        else if (Input.GetMouseButtonDown(0) && isMainMenuScenes == true)
+            Cursor.SetCursor(inGameCursorTap, hotspot, cursorMode);
+
         else if (Input.GetMouseButtonUp(0))
-                Cursor.SetCursor(inGameCursor, hotspot, cursorMode);
+            Cursor.SetCursor(inGameCursor, hotspot, cursorMode);
     }
 
     /* Moved these to update; OnMouse is only called when clicking an object's
